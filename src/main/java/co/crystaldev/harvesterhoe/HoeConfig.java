22d0c8eb733bff466aa8a706fae1af2956595445
phk01 <Patrickkolbech1@gmail.com>
2023-12-08 17:13:20 +0100

    idk trying to fix config being loaded too fast sometimes

--- src/main/java/co/crystaldev/harvesterhoe/HoeConfig.java
+++ src/main/java/co/crystaldev/harvesterhoe/HoeConfig.java
@@ -8,7 +8,7 @@ import org.bukkit.Material;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HoeConfig extends AlpineConfig {
+public final class HoeConfig extends AlpineConfig {
 
     public ConfigMessage noHoeMessage = new ConfigMessage("<red>You must be holding a hoe to harvest!");
     public ConfigMessage noOnlineMessage = new ConfigMessage("<red>That player is not online!");
@@ -16,15 +16,12 @@ public class HoeConfig extends AlpineConfig {
     public ConfigMessage giveMessage = new ConfigMessage("<green>You have been given a Harvester Hoe! %multiplier%");
     public ConfigMessage giveOtherMessage = new ConfigMessage("<green>You have given <yellow>%player%<green> a Harvester Hoe with multi %multiplier%!");
     @Comment("You can add more crops by doing \"CropsName: Amount\"")
-
     public Map<Material, Double> materialToAmount = new HashMap<>();
     // Constructor to initialize the map
-    public HoeConfig() {
-        // Populate the map with default material amounts
+    {
         materialToAmount.put(Material.WHEAT, 1.0);
         materialToAmount.put(Material.CARROTS, 1.2);
         materialToAmount.put(Material.POTATOES, 1.5);
-        // Add more materials and amounts as needed
     }
 
 }
